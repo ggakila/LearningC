3ba0119cb482543e0d12b4671d0251ea7b6b5eaf
ggakila <gisorehaggai@gmail.com>
2022-03-21 13:15:13 +0000

    Working with variables using char and int

---
+++ variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+ int main()
+{
+	char characterName[] = "Maliblue";
+	int characterAge = 3;
+	printf("There is a dog named %s\n", characterName);
+	printf("He is %d years old.\n", characterAge);
+	printf("He loves his name %s.\n", characterName);
+	printf("but not being %d.\n", characterAge);
+
+	return 0;
+}
